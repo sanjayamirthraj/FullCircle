3abaa75c6c2bb2ea218ce565f599dd45b2bc7b9c
Varun Vaidya <varun.vaidya@berkeley.edu>
2024-10-08 11:42:43 -0600

    toast working

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import { WagmiProvider } from 'wagmi';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 
 import { config } from '../wagmi';
+import { Toaster } from '@/components/ui/toaster';
 
 const client = new QueryClient();
 
@@ -16,6 +17,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <QueryClientProvider client={client}>
         <RainbowKitProvider>
           <Component {...pageProps} />
+          <Toaster />
         </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
